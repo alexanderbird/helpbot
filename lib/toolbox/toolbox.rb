1817b256c8535da2a6c24925102f41de92ddf800
Alexander Bird <alex.bird@arcurve.com>
2019-02-12 01:40:33 -0700

    Match commands case insensitively:

--- lib/toolbox/toolbox.rb
+++ lib/toolbox/toolbox.rb
@@ -1,6 +1,7 @@
 module Toolbox
   class Toolbox
-    def handle input
+    def handle raw_input
+      input = raw_input.downcase 
       toolClass = Tools.all.find {|toolClass| input.match(toolClass.will_handle) }
       (toolClass || Tools::Help).new(input).handle
     end
